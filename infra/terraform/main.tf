51080aa94a827aec663f9ca99a383c6c3c84f798
ajayyadav2181 <ajayyadav2181@gmail.com>
2026-08-12 15:41:06 +0530

    Terraforn Ab

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
     bucket       = "devops-accelerator-platform-tf-state-ajay"
     key          = "global/devops-accelerator/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = true    
+    dynamodb_table = "your-lock-table"    
     encrypt      = true     
   }
 }
